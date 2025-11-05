f9ef95013a98ee5d566df79b5c03249087eaa360
Leonidas Nwodo <chika@Leonidass-MacBook-Air.local>
2025-11-05 21:28:37 +0100

    Switch to Express and bind to 0.0.0.0:3000

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 const PORT = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-  res.end("Hello from Jenkins CI/CD pipeline!");
+app.get('/', (req, res) => {
+  res.send("Hello from Jenkins CI/CD pipeline!");
 });
 
-app.listen(3000, '0.0.0.0', () => {
-  console.log("Server running on port 3000");
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
